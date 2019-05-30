bf469d27a5b45e484e6b29383ec61ce219b1f0d7
shi-misch <shi9009chi@gmail.com>
2019-05-30 14:01:55 +0900

    test commit

---
+++ ColorGame.js
@@ -0,0 +1,34 @@
+alert("CONNECTED");
+var colors = [
+    "rgb(255, 0, 0)",　
+    "rgb(255, 255, 0)",　
+    "rgb(0, 255, 0)",　
+    "rgb(0, 255, 255)",　
+    "rgb(0, 0, 255)",　
+    "rgb(255, 0, 255)"　
+]
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++){
+    //add initinal colors to squares
+    squares[i].style.backgroundColor　= colors[i];
+
+    // add click listners to squares
+    squares[i].addEventListner("click", function(){
+        alert("clicked a square");
+        //grab color of clicked square
+        var clickedColor = this.style.backgroundColor;
+        //compare color to pickedColor
+        if (clickedColor === pickedColor) {
+            messageDisplay.textContent = "Correct!";
+        } else {
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again"
+        }
+    });
+}
